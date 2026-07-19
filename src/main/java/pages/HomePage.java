1ae58e9f7e2fa50371a0111a89204d3c395f7fd3
HP <narminosmanova63@gmail.com>
2026-07-19 18:21:03 +0400

    add to cart test

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -1,4 +1,47 @@
 package pages;
 
-public class HomePage {
-}
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+
+    public class HomePage extends BasePage {
+        public HomePage(WebDriver driver) {
+            super(driver);
+        }
+
+        private By addtocartButton = By.id("add-to-cart-sauce-labs-backpack");
+        private By carticon = By.className("shopping_cart_link");
+        private By lagoutButton=By.id("logout_sidebar_link");
+        private  By burgerMenu=By.id("react-burger-menu-btn");
+        private  By allItems=By.id("inventory_sidebar_link");
+        private By about=By.id("about_sidebar_link");
+        private By resetAppState=By.id("reset_sidebar_link");
+
+
+        public void adtocart() {
+            driver.findElement(addtocartButton).click();
+        }
+
+        public void opencart() {
+            driver.findElement(carticon).click();
+        }
+        public void clicklogoutButton(){
+            driver.findElement(lagoutButton).click();
+        }
+        public void clickburgermenyu(){
+            driver.findElement(burgerMenu).click();
+        }
+        public void clickAllItems(){
+            driver.findElement(allItems).click();
+        }
+        public void clickAbout(){
+            driver.findElement(about).click();
+        }
+        public void clickResetAppState(){
+            driver.findElement(resetAppState).click();
+        }
+
+    }
+
+
+
---
+++ src/main/java/usilis/TestData.java
@@ -0,0 +1,36 @@
+package usilis;
+
+import java.util.Random;
+
+
+    public class TestData {
+        Random random=new Random();
+
+        public String Fristname(){
+            return "nari"+random.nextInt(100);
+        }
+
+
+        // Login
+        public static final String USERNAME = "standard_user";
+        public static final String PASSWORD = "secret_sauce";
+
+        // URLs
+        public static final String LOGIN_URL = "https://www.saucedemo.com/";
+        public static final String INVENTORY_URL = "https://www.saucedemo.com/inventory.html";
+        public static final String CART_URL = "https://www.saucedemo.com/cart.html";
+        public static final String CHECKOUT_STEP_ONE_URL = "https://www.saucedemo.com/checkout-step-one.html";
+        public static final String CHECKOUT_STEP_TWO_URL = "https://www.saucedemo.com/checkout-step-two.html";
+        public static final String CHECKOUT_COMPLETE_URL = "https://www.saucedemo.com/checkout-complete.html";
+        public  static final String FALSE_CART_URL = "https://www.saucedemo.com/login.html";
+
+        // Checkout məlumatları
+        public static final String FIRST_NAME = "Narmin";
+        public static final String LAST_NAME = "Osmanova";
+        public static final String POSTAL_CODE = "AZ1000";
+
+        // Title
+        public static final String HOME_PAGE_TITLE = "Swag Labs";
+    }
+
+
