de1430ee062e1adafe339e1e86a76ead646c13de
Ramil Memmedov <memmedovramil780@gmail.com>
2026-07-19 22:08:41 +0400

    Checkout sehifesi ve testleri ugurla tamamlandi

---
+++ src/main/java/pages/CartPage.java
@@ -0,0 +1,12 @@
+package pages;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+public class CartPage extends BasePage {
+    private By checkoutButton = By.id("checkout");
+    public CartPage(WebDriver driver) {
+        super(driver);
+    }
+    public void clickCheckout() {
+        driver.findElement(checkoutButton).click();
+    }
+}
--- src/test/java/test/BaseTest.java
+++ src/test/java/test/BaseTest.java
@@ -1,16 +1,13 @@
-package tests;
+package test;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-import usilis.TestData;
-
+import utils.TestData; // 'usilis' sözü 'utils' olaraq düzəldildi (əgər paketin adı utils-dirsə)
 
 public class BaseTest {
-
     protected WebDriver driver;
-
     @BeforeMethod
     public void setUp() {
         driver = new ChromeDriver();
